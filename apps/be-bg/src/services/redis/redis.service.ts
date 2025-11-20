55a2da5b6f3674f68c868f5cd8c6d52fff0bd1c8
Mehdi <mehdilaz109@gmail.com>
2025-11-20 21:36:07 +0100

    Etablir la connection redis ainsi la duree de sauvegarde dans redis 2h

---
+++ apps/be-bg/src/services/redis/redis.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { OnModuleInit } from '@nestjs/common';
+import { Redis } from 'ioredis';
+
+/*Etablire la connection a redis
+  ansi les operations cache get et set */
+@Injectable()
+export class RedisService implements OnModuleInit{
+    private redisClient: Redis;
+    // Creation du client redis a l'initialisation du module
+
+    onModuleInit() {
+        this.redisClient = new Redis({
+            host: 'localhost',
+            port: 6379,
+        });
+    }
+    /**
+   * Récupère une valeur depuis Redis.
+   * @param key - clé du cache
+   */
+  async get<T>(key: string): Promise<T | null> {
+    const data = await this.redisClient.get(key);
+    return data ? (JSON.parse(data) as T) : null;
+  }
+
+  /**
+   * Sauvegarde une valeur dans Redis.
+   * @param key - clé du cache
+   * @param value - données à enregistrer
+   * @param ttl - durée de vie en secondes
+   */
+    async set<T>(key: string, value: unknown, ttl =  7200): Promise<void> {
+        await this.redisClient.set(key, JSON.stringify(value), 'EX', ttl);
+      }
+}
